af245bdef96eeda425afd3fda12cb1778f61965f
rahim <rahim.aghareb@gmail.com>
2021-05-17 09:54:01 +0430

    static files added

--- apgal/settings.py
+++ apgal/settings.py
@@ -126,5 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # static dir in local
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # static file for deployment
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
